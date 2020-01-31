ad12efc1f7193e0f1c9dbebf980a809d75e3db10
Rafael <rafac@unitec.edu>
2020-01-31 16:04:35 -0600

    Chequeo de Digitos ditintos: Karanprakar

--- main.cpp
+++ main.cpp
@@ -9,37 +9,69 @@
 
 using namespace std;
 
-int eNum1, eNum2, tamanio;
+int eNum1, eNum2, tamanio, powers = {1, 10, 100, 100};
+int *arrKaraprekar;.
 
 int getRand(){
 	int num = rand() % 100;
 	return num;
 }
 
+bool chkEquality(string buff){
+	int const tam = 4;
+	int num, intBuff[tam] = {-1,-1,-1,-1};
+	for(int i = 0; i < tam; i++){
+		num = stoi(buff[i]);
+		for(int j = 0; j < tam j++){
+			if (num == intBuff[j]){
+				return true;
+			}
+		}
+		intBuff[i] = num;
+		arrKaraprekar[i] = intBuff[i];
+	}
+	return false; 
+}
+
+void karaprekar(){
+	int res, num, numFalse;
+	while (res != 6174){
+		
+	}
+}
+
+int getDigits(){
+	while(true){
+		string buff;
+		cout << "Ingrese un Numero de cuatro cifras: \n";
+		cin >> buff;
+		if ( buff.length() != 4){
+			cout << "Debe ingresar un numero de 4 digitos!!!\n";
+		}else{
+			if (chkEquality){
+				cout << "Todos los digitos deben ser diferentes!!\n"
+			}
+			else{
+				break;
+			}
+		}
+	}
+	karaprekar();
+}
+
 int getNumber(int serie){
 	int num;
 	while (true){
-		if (serie){
-			cout << "Ingrese el " << serie << " numero para el MCD de Euclides: ";
-			cin >> num;
-			if (num > 1){
-				if(serie == 2 && eNum1 < num){
-					cout << "El " << serie << " debe ser menor que el " << serie -1 << " numero\n";
-				}else{
-					break;
-				}		
+		cout << "Ingrese el " << serie << " numero para el MCD de Euclides: ";
+		cin >> num;
+		if (num > 1){
+			if(serie == 2 && eNum1 < num){
+				cout << "El " << serie << " debe ser menor que el " << serie -1 << " numero\n";
 			}else{
-				cout << "Ingrese un numero NATURAL!!!\n";
-			}
+				break;
+			}		
 		}else{
-			string buff;
-			cout << "Ingrese un Numero de cuatro cifras: \n";
-			cin >> buff;
-			if ( buff.length() != 4){
-				cout << "Debe ingresar un numero de 4 digitos!!!";
-			}else{
-				
-			}
+			cout << "Ingrese un numero NATURAL!!!\n";
 		}
 	}
 	return num;
@@ -105,6 +137,7 @@ void sortear(){
 		arr[ref] = compare[0];
 		imprimir(arr, tamanio);
 	}
+	delete[] arr;
 }
 
 int menu(){
@@ -141,7 +174,7 @@ void selector(){
 				sortear();
 				break;
 			case 3:
-				cout << "El 4";
+				getDigits();
 				break;
 			case 0:
 				flag = true;
